0d086834b231a198dbcb21b1153d24460f1663a0
neha-15 <sindhujaravuri@gmail.com>
2020-07-12 20:39:47 +0530

    new folder rdip added

---
+++ RDIP/Codes/exp.js
@@ -0,0 +1,7 @@
+ function languagefun() {
+     document.getElementById('langval').innerHTML =
+         ("Form a  sentence (Declarative or Interrogative or any other type) from the given words")
+     document.getElementById('langval1').innerHTML =
+         ("select the buttons in proper order")
+
+ }
